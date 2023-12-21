aa95b3618b9a7f23b841a42b0dc1a85897d85d18
Renan Martins <renanmartins1999@hotmail.com>
2023-12-20 21:41:31 -0300

    fix: Correção do erro de serviço do MVC
    Issue: #3

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+    
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<LocalidadeController>();
 
